9cc04779f292ec632d5137d3010e0fa4a73da0cf
Kingslayer059 <154648646+Kingslayer059@users.noreply.github.com>
2025-02-04 21:36:10 -0500

    Modified player.py to support multiple OS's when opening banker.py

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+import shlex
 import sys
 import socket
+import platform
 from time import sleep
 import style as s
 from style import COLORS
@@ -24,6 +26,37 @@ def get_graphics():
     global text_dict
     text_dict = s.get_graphics()
 
+def banker_check():
+    has_passed_banker_query = False
+    is_banker = False
+    while(not has_passed_banker_query):
+        choice = input("If you would like to host a game, press b. If you would like to join a game, press p ")
+        if(choice == 'b' or choice == 'p'):
+            has_passed_banker_query = True
+            if(choice == 'b'):
+                is_banker = True
+        else:
+            ss.clear_screen()
+            print("Invalid choice, try again.")
+    ss.clear_screen()
+    if(is_banker == False):
+        return
+    current_os = platform.system()
+    if(current_os == "Windows"):
+        subprocess.call('start python banker.py', shell=True)
+    elif(current_os == "Darwin"):
+        cmd = "python banker.py"
+        subprocess.run(
+            shlex.split(
+            f"""osascript -e 'tell app "Terminal" to activate' -e 'tell app "Terminal" to do script "{cmd}" '"""
+            )
+        )   
+    elif(current_os == "Linux"):
+        subprocess.call(['gnome-terminal', '-x', 'python banker.py'])
+    else:
+        print("Current OS not supported to open new window, try running 'python banker.py' directly")
+    
+
 def initialize():
     """
     Initialize client receiver and sender network sockets, attempts to connect to a Banker by looping, then handshakes banker.
@@ -36,20 +69,7 @@ def initialize():
     """
     global sockets, ADDRESS, PORT
     ss.clear_screen()
-    has_passed_banker_query = False
-    is_banker = False
-    while(not has_passed_banker_query):
-        choice = input("If you would like to host a game, press b. If you would like to join a game, press p")
-        if(choice == 'b' or choice == 'p'):
-            has_passed_banker_query = True
-            if(choice == 'b'):
-                is_banker = True
-        else:
-            ss.clear_screen()
-            print("Invalid choice, try again.")
-    ss.clear_screen()
-    if(is_banker):
-        subprocess.call('start python banker.py', shell=True)
+    banker_check()
     print("Welcome to Terminal Monopoly, Player!")
     s.print_w_dots("Initializing client socket connection")     
     client_receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
