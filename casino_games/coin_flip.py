945c21da589926c99bf0a4b9469873397fa8a09a
emily garcia <emilygarcia@ufl.edu>
2025-02-17 17:07:03 -0500

    coin flip player issue, changed to dynamically update

--- casino_games/coin_flip.py
+++ casino_games/coin_flip.py
@@ -9,7 +9,8 @@ from style import COLORS as c
 game_title = "⛁ Coin Flip"
 header = "─" * ((75 - len(game_title)) // 2) + game_title + "─" * ((75 - len(game_title)) // 2)
 
-def play(active_terminal: Terminal, bet: int):
+
+def play(active_terminal: Terminal, player, bet: int):
     """
     Coin Flip
 
@@ -17,15 +18,15 @@ def play(active_terminal: Terminal, bet: int):
     A very simple base for a new casino_game
     Returns the wager to be sent to the player
     """
-    score = [0,0,0,0]
+    score = [0, 0, 0, 0]
 
     active_terminal.update(header + "\n" + g['coin_flip_heads'])
-    
-    choice = input(c.backYELLOW+c.BLACK+f"\rPlayer {1}: Heads or Tails? (h/T) ")
+
+    choice = input(c.backYELLOW + c.BLACK + f"\r{player.name}: Heads or Tails? (h/T) ")
     ss.overwrite("\r" + " " * 40)
     flip = random.choice(['heads', 'tails'])
-    
-    #TODO - Make the animation asynchrnous.
+
+    # TODO - Make the animation asynchronous.
     active_terminal.update(header + "\n" + g['coin_flip_heads'])
     sleep(0.2)
     active_terminal.update(header + "\n" + g['coin_flip_middle'])
@@ -36,22 +37,22 @@ def play(active_terminal: Terminal, bet: int):
     sleep(0.2)
     active_terminal.update(header + "\n" + g['coin_flip_heads' if flip == 'heads' else 'coin_flip_tails'])
 
-    if(choice.lower() == "h" and flip == 'heads'):
-        input(c.backYELLOW+c.BLACK+f"\rPlayer {1}: You got heads!")
+    if choice.lower() == "h" and flip == 'heads':
+        input(c.backYELLOW + c.BLACK + f"\r{player.name}: You got heads!")
         ss.overwrite("\r" + " " * 40)
         score[0] += 1
-    elif(choice.lower() == "h" and flip == 'tails'):
-        input(c.backYELLOW+c.BLACK+f"\rPlayer {1}: You got tails...")
+    elif choice.lower() == "h" and flip == 'tails':
+        input(c.backYELLOW + c.BLACK + f"\r{player.name}: You got tails...")
         ss.overwrite("\r" + " " * 40)
-    elif(flip == 'heads'):
-        input(c.backYELLOW+c.BLACK+f"\rPlayer {1}: You got heads...")
+    elif flip == 'heads':
+        input(c.backYELLOW + c.BLACK + f"\r{player.name}: You got heads...")
         ss.overwrite("\r" + " " * 40)
     else:
-        input(c.backYELLOW+c.BLACK+f"\rPlayer {1}: You got tails!")
+        input(c.backYELLOW + c.BLACK + f"\r{player.name}: You got tails!")
         ss.overwrite("\r" + " " * 40)
         score[0] += 1
 
-    if(score[0] == 1):
+    if score[0] == 1:
         active_terminal.update(header + "\n" + g['casino_win'])
         bet *= 2
     else:
